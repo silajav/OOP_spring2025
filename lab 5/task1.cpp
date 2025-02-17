3ef1e72b6c18b384d1822146793e5aef2b42a1af
silajav <silajawed@gmail.com>
2025-02-17 09:43:22 +0500

    Update task1.cpp

--- lab 5/task1.cpp
+++ lab 5/task1.cpp
@@ -1,1 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
 
+class car{
+    int num;//registration number.
+    string model;
+    string owner;
+     public:
+     car(int n, string m, string o){
+         num=n;
+         model=m;
+         owner=o;
+     }
+     car(car& obj, string str){
+         //the rest of the details are the same.
+         num=obj.num;
+         model=obj.model;
+         owner=str;
+     }
+     void display(){
+         cout<<"Registration number: "<<num<<endl;
+         cout<<"Model: "<<model<<endl;
+         cout<<"Owner: "<<owner<<endl;
+     }
+     
+};
+
+int main(){
+    //first object:
+    car obj1(1, "abc", "sil");
+    //second object:
+    car obj2(obj1, "def");
+    obj1.display();
+    obj2.display();
+}
